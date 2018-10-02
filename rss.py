ad136912719a7537cac36c871a9da57d6d5f7574
benjaminyuan <1459477412@qq.com>
2018-10-02 21:35:41 +0800

    selenium

--- rss.py
+++ rss.py
@@ -3,6 +3,8 @@ import time
 import os
 from bs4 import BeautifulSoup
 import re
+import json
+import csv
 #一些头部信息
 hds=[{'User-Agent':'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'},\
 {'User-Agent':'Mozilla/5.0 (Windows NT 6.2) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.12 Safari/535.11'},\
@@ -16,7 +18,7 @@ def film_spider(url,i):
     except:
         pass
 def get_Data(content):
-    soup = BeautifulSoup(content,'lxml')
+    soup = BeautifulSoup(content)
     container=[]
     for ele in soup.find_all('item'):
         titles = ele.find('title')
@@ -29,13 +31,40 @@ def get_Data(content):
         temp = [title,des,url]
         container.append(temp)
     return container
+class title_ob():
+    def __init__(self, title, des,url):
+        self.url = url
+        self.title = title
+        self.url = url
+            
 if __name__=='__main__':
     url = 'https://www.etw.fm/rss'
     content = film_spider(url,0)
     # print(content)
     reslut = get_Data(content)
     i = 1
+    #with open('rss.json','w') as fs:
+    all_dic = {}
+    temp = {}
+    index = 1
+    content_csv = open('rss.csv',"w",encoding='utf-8')
+    write  = csv.writer(content_csv)
+    write.writerow(['title','des','url','index'])
     for item in reslut:
+        temp.update({'title':item[0],'des':item[1],'src':item[2]})
+        write.writerow([item[0],item[1],item[2],index])
         print(f'标题：{item[0]}，des：{item[1]},url:{item[2]}')
         print('\n\n')
         print('-----------------------')
+        print(temp)
+        all_dic['title'+str(index)] = temp
+        temp={}
+        index+=1
+    print(all_dic)
+    print(type(all_dic))
+    print(type(json.dumps(all_dic)))
+    with open('rss.json','w',encoding='utf-8') as f:
+        content_json= json.dumps(all_dic)
+        json.dump(content_json,f)
+        f.write('\n')
+    f.close()
---
+++ submitForm.py
@@ -0,0 +1,46 @@
+from selenium import webdriver 
+import time
+import random
+def openChrome():
+    options = webdriver.ChromeOptions()
+    options.add_argument('disable-infobars')
+    driver = webdriver.Chrome(options= options)
+    return driver
+def operationAuth(driver):
+    url = "https://www.wjx.cn/m/28777538.aspx"
+    driver.get(url)
+    for i in range(1,7,1):
+        try:
+            time.sleep(2)
+            #   /div[@id="div{i}"]/div[@class="ui-controlgroup"]/a[@class="jqradio"]
+            # //*[@id="div12"]/ul/li[1]
+
+            elem = driver.find_elements_by_xpath(f'//*[@id="div{i}"]/div[@class="ui-controlgroup"]//a[@class="jqradio"]')
+            print(elem)
+            print(f'第{i}次')
+            elem[0].click()
+        except:
+            pass
+    for j in range(7,12,1):
+        try:
+            checks = driver.find_elements_by_xpath(f'//*[@id="div{j}"]/div[@class="ui-controlgroup"]//a[@class="jqcheck"]')
+            print(checks)
+            for item in range(0,len(checks)-1):
+                checks[item].click()
+        except:
+            pass
+    checks = driver.find_elements_by_xpath('//*[@id="div12"]/ul/li')
+    print(checks)
+    for item in range(0,len(checks)):
+                time.sleep(1)
+                checks[item].click()
+    print('操作完毕')
+ 
+    text1=driver.find_element_by_xpath('//*[@id="q13"]')
+    text2=driver.find_element_by_xpath('//*[@id="q14"]')
+    text1.send_keys("来自班长的恶搞")
+    text2.send_keys("来自班长的恶搞")
+    driver.find_element_by_xpath('//*[@id="ctlNext"]').click()
+if __name__=="__main__":
+    driver = openChrome()
+    operationAuth(driver)
